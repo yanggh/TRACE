cddd1727241801d2f78b770dd4d582ab8acf5b7a
yanggh <823457590@qq.com>
2018-05-07 14:14:49 +0800

    rm -f *.o

---
+++ Daemon_init.cpp
@@ -0,0 +1,45 @@
+#include "Daemon_init.h"
+#include "error.h"
+//#include "wrapsignal.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define	MAXFD	64
+
+int
+Daemon_init()
+{
+	int		i;
+	pid_t	pid;
+
+	if ( (pid = fork()) < 0)
+		err_sys("fork error");
+	else if (pid)
+		_exit(0);			/* parent terminates */
+
+	/* child 1 continues... */
+
+	if (setsid() < 0)			/* become session leader */
+		return (-1);
+
+//	Signal(SIGHUP, SIG_IGN);
+	if ( (pid = fork()) < 0)
+		err_sys("fork error");
+	else if (pid)
+		_exit(0);			/* child 1 terminates */
+
+	/* child 2 continues... */
+
+	/* close off file descriptors */
+	for (i = 0; i < MAXFD; i++)
+		close(i);
+
+	/* redirect stdin, stdout, and stderr to /dev/null */
+	open("/dev/null", O_RDONLY);
+	open("/dev/null", O_RDWR);
+	open("/dev/null", O_RDWR);
+
+	return (0);				/* success */
+}
---
+++ Daemon_init.h
@@ -0,0 +1,6 @@
+#ifndef __unp_daemon_init_h
+#define __unp_daemon_init_h
+
+int Daemon_init();
+
+#endif
